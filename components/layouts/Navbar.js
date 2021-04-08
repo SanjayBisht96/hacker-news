88dbf1259c8de64567c52048afa2dac46d02a18d
Sachin <sachinkshuklaoo7@gmail.com>
2021-04-08 18:10:25 +0530

    publish posting UI ready

--- components/layouts/Navbar.js
+++ components/layouts/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
         <a href="/" className="navbar__logo__link">Hacker News</a>
       </div>
       <div className="navbar__links">
-        <a href="#" className="navbar__links__item">
+        <a href="/post" className="navbar__links__item">
           Create Post
         </a>
         <a href="/job" className="navbar__links__item">
--- pages/post.js
+++ pages/post.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/layouts/Navbar";
 
-const post = () => {
-    return (
-        <div>
-            
+const PublishAPost = () => {
+  return (
+    <main className="posting">
+      <Navbar />
+
+      <section className="posting__container">
+        <div className="posting__container__content">
+          <h3 className="heading-sub--sub">Submit A Post</h3>
+          <form className="form posting__container__content__form">
+            <div className="form__group posting__container__content__form__group">
+              <label className="form__label">Post Title *</label>
+              <input
+                type="text"
+                className="form__input posting__container__content__form__input"
+              />
+            </div>
+            <div className="form__group posting__container__content__form__group">
+              <label className="form__label">Post URL *</label>
+              <input
+                type="text"
+                className="form__input posting__container__content__form__input"
+              />
+            </div>
+            <div className="form__group posting__container__content__form__group">
+              <label className="form__label">Post Tags</label>
+              <input
+                type="text"
+                className="form__input posting__container__content__form__input"
+              />
+              <p className="paragraph--sub">*To add tags, start tags by ‘#’ and end with a single space.</p>
+            </div>
+            <button className="btn btn-md form__submit posting__container__content__form__submit">
+              Send for approval
+            </button>
+          </form>
         </div>
-    )
-}
+      </section>
+    </main>
+  );
+};
 
-export default post
+export default PublishAPost;
